4724f610653b4892604345950e66bf1732546ccb
Cristiano Nascimento <cnascimento@korewa.com.br>
2012-05-04 17:35:55 -0300

    alterações final sem css

--- public/javascripts/funcoes.js
+++ public/javascripts/funcoes.js
@@ -6,38 +6,18 @@ $(document).ready(function() {
 		if($("#botao").hasClass("closed"))
 		{
 		  $("#Menu").slideDown();
-		  $("#botao").attr("src", "/images/menu/top_loginbar_abaup.png");
 		  $("#botao").removeClass("closed");
 		  $("#botao").addClass("open");
 		}
 		else
 		{
 			$("#Menu").slideUp();
-		  	$("#botao").attr("src", "/images/menu/top_loginbar_abadown.png");
 			$("#botao").removeClass("open");
 		  	$("#botao").addClass("closed");
 		}
 	});
 	
-	$("#Menu1").hide();
-//	$("#botao").addClass("closed");  
-	
-	$("#botao1").click(function(){
-		if($("#botao1").hasClass("closed"))
-		{
-		  $("#Menu1").slideDown();
-		  $("#botao1").attr("src", "/images/menu/top_menu_abaup.png");
-		  $("#botao1").removeClass("closed");
-		  $("#botao1").addClass("open");
-		}
-		else
-		{
-			$("#Menu1").slideUp();
-		  	$("#botao1").attr("src", "/images/menu/top_menu_abadown.png");
-			$("#botao1").removeClass("open");
-		  	$("#botao1").addClass("closed");
-		}
-	});
+
 	
 	 $(".p").mouseover(function() {
       	//alert("a");
