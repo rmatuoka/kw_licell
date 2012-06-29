4795835df75ffaf0cd9d2fed335ddbf587cafb54
-_- Zaba -_- <bsilva@korewa.com.br>
2012-06-29 19:58:52 -0300

    ajustes de email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,17 +1,17 @@
 class UserMailer < ActionMailer::Base
-  default :from => "Licell - Vendas <vendas@licell.com.br>"
+  default :from => "Licell - Vendas <no-reply@licell.com.br>"
   
   #Enviado quando ocorrer o primeiro retorno do pagseguro!
   def transaction_initiated(order)
     @order = order
-    mail(:to => order.user.email, :subject => "Transação Iniciada")
+    mail(:to => order.user.email, :subject => "Transação Iniciada",:reply_to => "daniel@licell.com.br")
   end
   
   #Enviado quando o pagamento for concuido!
   def payment_made(order)
     @order = order
     @order_itens = OrderProduct.all(:conditions => ['order_id = ?', @order.id])
-    mail(:to => order.user.email, :subject => "Pedido Efetuado")
+    mail(:to => order.user.email, :subject => "Pedido Efetuado",:reply_to => "daniel@licell.com.br")
   end
   
   #Enviado quando o código de rastreamento for inserido na tabela order!
@@ -31,14 +31,14 @@ class UserMailer < ActionMailer::Base
   def order_start(order)
     @order = order
     @order_itens = OrderProduct.all(:conditions => ['order_id = ?', @order.id])    
-    mail(:to=>"daniel@licell.com.br", :bcc => "log@korewa.com.br", :subject => @order.user.nome + " - Novo Pedido através do Site")
+    mail(:to=>"daniel@licell.com.br", :bcc => "sussumu@hdcomputerscom.br, log@korewa.com.br", :subject => @order.user.nome + " - Novo Pedido através do Site")
   end
   
   #Enviado quando o pagamento for concluido!
   def order_completed(order)
     @order = order
     @order_itens = OrderProduct.all(:conditions => ['order_id = ?', @order.id])    
-    mail(:to=>"daniel@licell.com.br", :bcc => "log@korewa.com.br", :subject => @order.user.nome + " - Pagamento Efetuado")    
+    mail(:to=>"daniel@licell.com.br", :bcc => "sussumu@hdcomputerscom.br, log@korewa.com.br", :subject => @order.user.nome + " - Pagamento Efetuado")    
   end
   
   #Enviado quando o tem algum comentário em algum produto
@@ -46,12 +46,12 @@ class UserMailer < ActionMailer::Base
     @user = user
     @product = product
     
-    mail(:to=>"daniel@licell.com.br", :bcc => "log@korewa.com.br", :subject => @user.nome + " comentou no produto " + @product.name)    
+    mail(:to=>"daniel@licell.com.br", :bcc => "sussumu@hdcomputerscom.br, log@korewa.com.br", :subject => @user.nome + " comentou no produto " + @product.name)    
   end
   
   def reports(vendasok)
      @vendasefetuadas = vendasok
-     mail(:to => "daniel@licell.com.br", :bcc => "log@korewa.com.br", :subject => "Relatorio de Vendas aprovadas!")
+     mail(:to => "daniel@licell.com.br", :bcc => "sussumu@hdcomputerscom.br, log@korewa.com.br", :subject => "Relatorio de Vendas aprovadas!")
    end
   
 end
