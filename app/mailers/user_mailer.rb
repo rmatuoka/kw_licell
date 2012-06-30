96a181899b7f15c0cd16823e54710186ac9cd285
-_- Zaba -_- <bsilva@korewa.com.br>
2012-06-29 22:52:42 -0300

    Ajuste no setup mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -31,14 +31,14 @@ class UserMailer < ActionMailer::Base
   def order_start(order)
     @order = order
     @order_itens = OrderProduct.all(:conditions => ['order_id = ?', @order.id])    
-    mail(:to=>"daniel@licell.com.br", :bcc => "sussumu@hdcomputerscom.br, log@korewa.com.br", :subject => @order.user.nome + " - Novo Pedido através do Site")
+    mail(:to=>"daniel@licell.com.br", :bcc => "sussumu@hdcomputers.com.br, log@korewa.com.br", :subject => @order.user.nome + " - Novo Pedido através do Site")
   end
   
   #Enviado quando o pagamento for concluido!
   def order_completed(order)
     @order = order
     @order_itens = OrderProduct.all(:conditions => ['order_id = ?', @order.id])    
-    mail(:to=>"daniel@licell.com.br", :bcc => "sussumu@hdcomputerscom.br, log@korewa.com.br", :subject => @order.user.nome + " - Pagamento Efetuado")    
+    mail(:to=>"daniel@licell.com.br", :bcc => "sussumu@hdcomputers.com.br, log@korewa.com.br", :subject => @order.user.nome + " - Pagamento Efetuado")    
   end
   
   #Enviado quando o tem algum comentário em algum produto
@@ -46,12 +46,12 @@ class UserMailer < ActionMailer::Base
     @user = user
     @product = product
     
-    mail(:to=>"daniel@licell.com.br", :bcc => "sussumu@hdcomputerscom.br, log@korewa.com.br", :subject => @user.nome + " comentou no produto " + @product.name)    
+    mail(:to=>"daniel@licell.com.br", :bcc => "sussumu@hdcomputers.com.br, log@korewa.com.br", :subject => @user.nome + " comentou no produto " + @product.name)    
   end
   
   def reports(vendasok)
      @vendasefetuadas = vendasok
-     mail(:to => "daniel@licell.com.br", :bcc => "sussumu@hdcomputerscom.br, log@korewa.com.br", :subject => "Relatorio de Vendas aprovadas!")
+     mail(:to => "daniel@licell.com.br", :bcc => "sussumu@hdcomputers.com.br, log@korewa.com.br", :subject => "Relatorio de Vendas aprovadas!")
    end
   
 end
